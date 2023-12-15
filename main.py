2eec75d2032b17e87c3edc869a0de27fca7a9643
kkvantas <krizzivan66@gmail.com>
2023-12-15 19:36:26 +0100

    Improve data pipeline functions, add docstrings, add API documentation

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
 import io
-from fastapi import FastAPI, File
-from typing_extensions import Annotated
+from fastapi import FastAPI, HTTPException, UploadFile
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-from numpy import asarray
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
-def load_model(file_name):
-    return tf.keras.models.load_model(file_name)
-
-
-def get_predict(model, X):
-    y_hat = np.argmax(model.predict(X), axis=1)
-    print(np.shape(y_hat), y_hat)
+def get_prediction(model, x):
+    """
+    Gets the predicted digit
+    :param model: MNIST Handwritten Digit Classification keras model instance
+    :param x: input array
+    :type: np.ndarray
+    :return: predicted digit
+    :rtype: int
+    """
+    y_hat = np.argmax(model.predict(x), axis=1)
     return int(y_hat[0])
 
 
-def get_matrix(img):
-    image = Image.open(img).convert('L').resize((28, 28), reducing_gap=2.0)
-    as_array = asarray(image)
-    matrix = as_array.reshape(1, 784)
-    return matrix
+def convert_image_data_to_input_array(image_io):
+    """
+    Converts image data to input array
+    :param image_io: image io object
+    :return: array
+    :rtype: np.ndarray
+    """
+    image = Image.open(image_io).convert('L').resize((28, 28), reducing_gap=2.0)
+    return np.asarray(image).reshape(1, 784)
 
 
-@app.post("/predict")
-async def create_file(file:  Annotated[bytes, File()]):
-    bytes_io = io.BytesIO(file)
-    matrix = get_matrix(bytes_io)
-    predict = get_predict(load_model('model.h5'), matrix)
-    return {"result": predict}
+class PredictionResult(BaseModel):
+    result: int
+
+
+@app.post("/", response_model=PredictionResult, summary='Predicts digit from an input image')
+async def predict(file: UploadFile):
+    if not file.content_type.startswith('image/'):
+        raise HTTPException(status_code=400, detail=f"File '{file.filename}' is not an image.")
+    contents = await file.read()
+    matrix = convert_image_data_to_input_array(io.BytesIO(contents))
+    prediction = get_prediction(tf.keras.models.load_model('model.h5'), matrix)
+    return {"result": prediction}
+
